67d9d1b3bc5ab96d0c6a3bcc4ebc39f96d38a046
DimaBorek <drackon4ik@gmail.com>
2018-04-03 21:13:04 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,13 +9,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{6,4,5, 4, 3, 2, -9, 4};
+        int[] arr = new int[]{3, 2, 6, 7, 9, 4};
         bubbleSort(arr);
     }
     public static void bubbleSort(int[]arr){
     for (int i = arr.length-1;i>0;i--){
             for (int j = 0; j < i; j++) {
-                if(arr[j]<arr[j+1]){
+                if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1]=temp;
@@ -28,4 +28,18 @@ public class Main {
 
         }
     }
+    private static int getMaxFromArray(int[] array){
+        int indexOfMax = 0;
+        int array1[] = new int[]{4,9,3,5};
+        for (int i = 1; i < array1.length ; i++) {
+            if (array[i]>array[indexOfMax]){
+                indexOfMax = i;
+            }
+
+        }
+        return array[indexOfMax];
+
+    }
+
+
 }
